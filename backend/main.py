4ee9d02998f9c7a7d1486eadce2a1d069a0a4b2f
edrassimoes <edrassimoes@gmail.com>
2024-12-06 11:35:06 -0300

    atualizando api

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from datetime import datetime
+import asyncpg
+import os
+
+async def get_database():
+    DATABASE_URL = os.environ.get("PGURL", "postgres://postgres:postgres@db:5432/jogos")
+    return await asyncpg.connect(DATABASE_URL)
 
 app = FastAPI()
 
@@ -17,44 +23,48 @@ class Pedido(BaseModel):
     forma_pagamento: str
     horario: datetime = datetime.now()
 
-# ----------------------------------------------- PRATOS ---------------------------------------------------------------
+# -----------------------------------------------  CRUD PRATOS ---------------------------------------------------------
 @app.post("api/v1/pratos/")
-def criar_prato(prato: Prato):
+async def criar_prato(prato: Prato):
     return {"message": "Prato criado"}
 
 @app.get("api/v1/pratos/")
-def listar_pratos():
+async def listar_pratos():
     return {"message": "Lista de pratos"}
 
 @app.get("api/v1/pratos/{id}")
-def listar_prato(id: int):
+async def listar_prato(id: int):
     return {"message": f"Prato {id} encontrado"}
 
-@app.put("api/v1/pratos/{id}")
-def atualizar_prato(id: int, prato: Prato):
+@app.patch("api/v1/pratos/{id}")
+async def atualizar_prato(id: int, prato: Prato):
     return {"message": f"Prato {id} atualizado"}
 
 @app.delete("/api/v1/pratos/{id}")
-def deletar_prato(id: int):
+async def deletar_prato(id: int):
     return {"message": f"Prato {id} deletado"}
 
-# ----------------------------------------------- PEDIDOS --------------------------------------------------------------
+# ----------------------------------------------- CRUD PEDIDOS ---------------------------------------------------------
 @app.post("api/v1/pedidos/")
-def criar_pedido(pedido: Pedido):
+async def criar_pedido(pedido: Pedido):
     return {"message": "Pedido criado"}
 
 @app.get("api/v1/pedidos/")
-def listar_pedidos():
+async def listar_pedidos():
     return {"message": "Lista de pedidos"}
 
 @app.get("api/v1/pedidos/{id}")
-def listar_pedido(id: int):
+async def listar_pedido(id: int):
     return {"message": f"Pedido {id} encontrado"}
 
-@app.put("api/v1/pedidos/{id}")
-def atualizar_pedido(id: int, pedido: Pedido):
+@app.patch("api/v1/pedidos/{id}")
+async def atualizar_pedido(id: int, pedido: Pedido):
     return {"message": f"Pedido {id} atualizado"}
 
 @app.delete("/api/v1/pedidos/{id}")
-def deletar_pedido(id: int):
+async def deletar_pedido(id: int):
     return {"message": f"Pedido {id} deletado"}
+# ----------------------------------------------- RESET DO BANCO -------------------------------------------------------
+@app.delete("/api/v1/pratos/")
+async def reset_data():
+    return {"message": "Banco de dados resetado"}
